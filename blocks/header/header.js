6d162ca4297d657ef821e351df3a79b1c5d6d614
adls <student@example.com>
2026-08-19 21:18:01 +0000

    resolved errors

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -137,13 +137,12 @@ export default async function decorate(block) {
     brandLink.closest('.button-container').className = '';
   }
   const logo = navBrand.querySelector('picture');
-if (logo) {
- // Replace the first section's contents with the authored image wrapped with a link to '/'
- navBrand.innerHTML = `<a href="/" aria-label="Home" title="Home" class="home">${logo.outerHTML}</a>`;
- // Make sure the logo is not lazy loaded as it's above the fold and can affect page load speed
- navBrand.querySelector('img').settAttribute('loading', 'eager');
- }
-
+  if (logo) {
+  // Replace the first section's contents with the authored image wrapped with a link to '/'
+    navBrand.innerHTML = `<a href="/" aria-label="Home" title="Home" class="home">${logo.outerHTML}</a>`;
+    // Make sure the logo is not lazy loaded as it's above the fold and can affect page load speed
+    navBrand.querySelector('img').settAttribute('loading', 'eager');
+  }
 
   const navSections = nav.querySelector('.nav-sections');
   if (navSections) {
